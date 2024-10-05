583ed3b52f1201351188b822f92189e2d14084b7
Samuel Andert <samuel@andert.me>
2024-10-05 16:31:20 +0200

    WIP: refactor to SSR auth part2

--- apps/auth/src/routes/+page.svelte
+++ apps/auth/src/routes/+page.svelte
@@ -16,13 +16,8 @@
   };
 </script>
 
-<svelte:head>
-  <title>User Management</title>
-</svelte:head>
-
 <form class="row flex flex-center" method="POST" use:enhance={handleSubmit}>
   <div class="col-6 form-widget">
-    <h1 class="header">Supabase + SvelteKit</h1>
     <p class="description">Sign in via magic link with your email below</p>
     {#if form?.message !== undefined}
       <div class="success {form?.success ? '' : 'fail'}">
--- apps/auth/src/routes/account/+page.svelte
+++ apps/auth/src/routes/account/+page.svelte
@@ -4,24 +4,11 @@
   import type { SubmitFunction } from "@sveltejs/kit";
 
   export let data;
-  export let form;
 
-  let { session, supabase, profile } = data;
-  $: ({ session, supabase, profile } = data);
+  let { session } = data;
+  $: ({ session } = data);
 
-  let profileForm: HTMLFormElement;
   let loading = false;
-  let fullName: string = profile?.full_name ?? "";
-  let username: string = profile?.username ?? "";
-  let website: string = profile?.website ?? "";
-  let avatarUrl: string = profile?.avatar_url ?? "";
-
-  const handleSubmit: SubmitFunction = () => {
-    loading = true;
-    return async () => {
-      loading = false;
-    };
-  };
 
   const handleSignOut: SubmitFunction = () => {
     loading = true;
@@ -33,57 +20,10 @@
 </script>
 
 <div class="form-widget">
-  <form
-    class="form-widget"
-    method="post"
-    action="?/update"
-    use:enhance={handleSubmit}
-    bind:this={profileForm}
-  >
-    <div>
-      <label for="email">Email</label>
-      <input id="email" type="text" value={session.user.email} disabled />
-    </div>
-
-    <div>
-      <label for="fullName">Full Name</label>
-      <input
-        id="fullName"
-        name="fullName"
-        type="text"
-        value={form?.fullName ?? fullName}
-      />
-    </div>
-
-    <div>
-      <label for="username">Username</label>
-      <input
-        id="username"
-        name="username"
-        type="text"
-        value={form?.username ?? username}
-      />
-    </div>
-
-    <div>
-      <label for="website">Website</label>
-      <input
-        id="website"
-        name="website"
-        type="url"
-        value={form?.website ?? website}
-      />
-    </div>
-
-    <div>
-      <input
-        type="submit"
-        class="button block primary"
-        value={loading ? "Loading..." : "Update"}
-        disabled={loading}
-      />
-    </div>
-  </form>
+  <div>
+    <label for="email">Email</label>
+    <input id="email" type="text" value={session.user.email} disabled />
+  </div>
 
   <form method="post" action="?/signout" use:enhance={handleSignOut}>
     <div>
