1ed2b507e6fa660cced0cdd3db3e24fd30ec5571
Samuel Andert <samuel@andert.me>
2024-10-08 14:10:50 +0200

    testing next wundergraph env vars deployment config 13

--- apps/api/.wundergraph/wundergraph.config.ts
+++ apps/api/.wundergraph/wundergraph.config.ts
@@ -2,14 +2,10 @@ import {
   configureWunderGraphApplication,
   cors,
   EnvironmentVariable,
-  introspect,
   templates,
 } from "@wundergraph/sdk";
 import server from "./wundergraph.server";
 import operations from "./wundergraph.operations";
-
-const isProduction = process.env.NODE_ENV === "production";
-const isNext = process.env.NEXT_PUBLIC_VERCEL_ENV === "preview";
 
 configureWunderGraphApplication({
   apis: [],
@@ -27,29 +23,29 @@ configureWunderGraphApplication({
     tokenBased: {
       providers: [
         {
-          userInfoEndpoint: isProduction
-            ? "https://visioncreator.earth/auth/userinfo"
-            : isNext
-            ? "https://next.visioncreator.earth/auth/userinfo"
-            : "http://127.0.0.1:3000/auth/userinfo",
+          userInfoEndpoint:
+            process.env.VERCEL_ENV === "preview"
+              ? "https://next.visioncreator.earth/auth/userinfo"
+              : "http://127.0.0.1:3000/auth/userinfo",
         },
       ],
     },
   },
   cors: {
     ...cors.allowAll,
-    allowedOrigins: isProduction
-      ? ["https://visioncreator.earth"]
-      : isNext
-      ? ["https://next.visioncreator.earth"]
-      : ["http://127.0.0.1:3000", new EnvironmentVariable("WG_ALLOWED_ORIGIN")],
+    allowedOrigins:
+      process.env.VERCEL_ENV === "preview"
+        ? ["https://next.visioncreator.earth"]
+        : [
+            "http://127.0.0.1:3000",
+            new EnvironmentVariable("WG_ALLOWED_ORIGIN"),
+          ],
   },
   options: {
-    publicNodeUrl: isProduction
-      ? "https://api-visioncreator-earth.fly.dev"
-      : isNext
-      ? "https://api-next-visioncreator-earth.fly.dev"
-      : "http://127.0.0.1:9991",
+    publicNodeUrl:
+      process.env.VERCEL_ENV === "preview"
+        ? "https://api-next-visioncreator-earth.fly.dev"
+        : "http://127.0.0.1:9991",
   },
   authorization: {
     roles: ["admin", "authenticated"],
