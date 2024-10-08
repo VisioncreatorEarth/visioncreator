cfd41ec2feaa06e7d48a11afcbffed41d52070dd
Samuel Andert <samuel@andert.me>
2024-10-08 17:53:13 +0200

    testing wundergraph NEXT env deploy 14

--- apps/api/.wundergraph/wundergraph.config.ts
+++ apps/api/.wundergraph/wundergraph.config.ts
@@ -23,25 +23,17 @@ configureWunderGraphApplication({
     tokenBased: {
       providers: [
         {
-          userInfoEndpoint: process.env.WG_USER_INFO_ENDPOINT
-            ? process.env.WG_USER_INFO_ENDPOINT
-            : new EnvironmentVariable("WG_USER_INFO_ENDPOINT"),
+          userInfoEndpoint: new EnvironmentVariable("WG_USER_INFO_ENDPOINT"),
         },
       ],
     },
   },
   cors: {
     ...cors.allowAll,
-    allowedOrigins: [
-      process.env.WG_ALLOWED_ORIGIN
-        ? process.env.WG_ALLOWED_ORIGIN
-        : new EnvironmentVariable("WG_ALLOWED_ORIGIN"),
-    ],
+    allowedOrigins: [new EnvironmentVariable("WG_ALLOWED_ORIGIN")],
   },
   options: {
-    publicNodeUrl: process.env.WG_PUBLIC_NODE_URL
-      ? process.env.WG_PUBLIC_NODE_URL
-      : new EnvironmentVariable("WG_PUBLIC_NODE_URL"),
+    publicNodeUrl: new EnvironmentVariable("PUBLIC_WG_API_URL"),
   },
   authorization: {
     roles: ["admin", "authenticated"],
