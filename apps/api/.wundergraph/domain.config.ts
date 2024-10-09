f556203d0030822fd11e1db033d12e787d604ed8
Samuel Andert <samuel@andert.me>
2024-10-09 13:30:29 +0200

    testing wundergraph next env deploy 20

--- apps/api/.wundergraph/domain.config.ts
+++ apps/api/.wundergraph/domain.config.ts
@@ -1,3 +1,16 @@
+console.log(
+  "----------external process env NANGO------------",
+  process.env.NANGO_HOST
+);
+console.log(
+  "----------external process env VERCEL_------------",
+  process.env.VERCEL_ENV
+);
+console.log(
+  "----------external process env NODE_ENV------------",
+  process.env.NODE_ENV
+);
+
 export const domainConfig = {
   userInfoEndpoint: "http://127.0.0.1:3000/auth/userinfo",
   allowedOrigins: ["http://127.0.0.1:3000"],
