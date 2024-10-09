6e6db4de7d5f9075379ea6b5e5065c20d68a844d
Samuel Andert <samuel@andert.me>
2024-10-09 18:26:24 +0200

    WIP: VideoGrid Player

---
+++ apps/app/src/routes/me/compose/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import ComposeView from '$lib/components/ComposeView.svelte';
+
+	// Get the component name from the URL query parameter
+	$: componentName = $page.url.searchParams.get('c') || 'HelloEarth';
+
+	// Create the view object with the dynamic component name
+	$: view = {
+		id: 'HelloEarth',
+		layout: {
+			areas: `
+                "main"
+            `
+		},
+		children: [
+			{
+				id: 'xyz1',
+				component: componentName,
+				slot: 'main'
+			}
+		]
+	};
+</script>
+
+<ComposeView {view} />
