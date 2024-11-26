ba1721eb93fa79da7d8095efb68844bfa9913b56
Samuel Andert <samuel@andert.me>
2024-11-26 18:09:00 +0100

    fixing frontend deploy bugs

--- apps/app/src/lib/components/TabMenu.svelte
+++ apps/app/src/lib/components/TabMenu.svelte
@@ -19,29 +19,10 @@
 			name: 'Episodes',
 			icon: 'mdi:play-circle',
 			href: '/episodes'
-		},
-		{
-			name: 'BringMe',
-			icon: 'mdi:cart',
-			href: '/me',
-			class: 'variant-ghost-tertiary',
-			onClick: () => {
-				dynamicView.set({
-					view: {
-						id: "bring",
-						component: "o-Bring",
-						layout: {
-							areas: "'content'",
-							columns: "1fr",
-							rows: "1fr"
-						}
-					}
-				});
-			}
 		}
 	];
 
-	async function handleNavigation(link: typeof viewLinks[number]) {
+	async function handleNavigation(link: (typeof viewLinks)[number]) {
 		if (link.onClick) {
 			link.onClick();
 		}
@@ -51,18 +32,19 @@
 </script>
 
 <div class="flex flex-col h-full">
-	<div class="flex-grow p-4 overflow-auto">
+	<div class="overflow-auto flex-grow p-4">
 		{#if activeTab === 'views'}
 			<div class="flex gap-4">
 				{#each viewLinks as link}
 					<div
 						on:click={() => handleNavigation(link)}
 						on:keydown={(e) => e.key === 'Enter' && handleNavigation(link)}
-						class="flex flex-col items-center justify-center w-[100px] h-[100px] transition-colors duration-200 rounded-lg cursor-pointer {link.class || 'variant-ghost-secondary'} hover:variant-ghost-primary"
+						class="flex flex-col items-center justify-center w-[100px] h-[100px] transition-colors duration-200 rounded-lg cursor-pointer {link.class ||
+							'variant-ghost-secondary'} hover:variant-ghost-primary"
 						tabindex="0"
 						role="button"
 					>
-						<Icon icon={link.icon} class="w-8 h-8 mb-2" />
+						<Icon icon={link.icon} class="mb-2 w-8 h-8" />
 						<span class="text-sm font-medium">{link.name}</span>
 					</div>
 				{/each}
@@ -72,12 +54,12 @@
 		{/if}
 	</div>
 
-	<div class="flex items-center justify-between p-2 pr-16 border-t border-surface-500">
+	<div class="flex justify-between items-center p-2 pr-16 border-t border-surface-500">
 		<ul class="flex flex-wrap text-sm font-medium text-center">
 			{#each tabs as tab}
 				<li class="relative px-0.5 sm:px-1">
 					<button
-						class="inline-block px-2 py-2 transition-colors duration-200 rounded-lg sm:px-3"
+						class="inline-block px-2 py-2 rounded-lg transition-colors duration-200 sm:px-3"
 						class:text-primary-500={activeTab === tab}
 						class:text-tertiary-400={activeTab !== tab}
 						on:click={() => dispatch('setActiveTab', tab)}
@@ -86,7 +68,7 @@
 					</button>
 					{#if activeTab === tab}
 						<div
-							class="absolute bottom-0 left-1/2 -translate-x-1/2 w-1/4 h-0.5 bg-primary-500 rounded-full"
+							class="absolute bottom-0 left-1/2 w-1/4 h-0.5 rounded-full -translate-x-1/2 bg-primary-500"
 						/>
 					{/if}
 				</li>
--- apps/app/src/routes/local/bringme/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  import ComposeView from '$lib/components/ComposeView.svelte';
-  import { view } from '$lib/views/BringMe';
-</script>
-
-<ComposeView {view} />
