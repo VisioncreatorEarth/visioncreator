e0b3d7f76acab75f184d3f6d0dffcde881afec98
Samuel Andert <samuel@andert.me>
2024-11-29 17:16:29 +0100

    Playing with Background Gradients

---
+++ apps/app/src/routes/local/view/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import BackgroundGradient from '$lib/components/BackgroundGradient.svelte';
+</script>
+
+<BackgroundGradient />
+
+<div class="relative min-h-screen">
+	<!-- Your content here -->
+</div>
