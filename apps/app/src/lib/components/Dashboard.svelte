dbb88686b64615b9c82cb9bac980cc5115e08bed
Samuel Andert <samuel@andert.me>
2024-12-05 11:51:02 +0100

    Finetuning dashboard for mobile responsiveness

--- apps/app/src/lib/components/Dashboard.svelte
+++ apps/app/src/lib/components/Dashboard.svelte
@@ -11,9 +11,7 @@
 	let showQRCode = writable(false);
 	let linkCopied = writable(false);
 
-	let localInspirationsCount = 0;
-
-	$: inspirations = localInspirationsCount || Number($query.data.inspirations) || 0;
+	$: inspirations = $query.data.inspirations;
 	$: fullyUnlocked = inspirations >= 3;
 	$: invitationLink = `${env.PUBLIC_BASE_URL}/?visionid=${$query.data.qrCodeId}`;
 	$: temperature = $query.data.temperature;
@@ -40,22 +38,6 @@
 		await $query.refetch();
 	}
 
-	function decrementLocal() {
-		if (localInspirationsCount > 0) localInspirationsCount--;
-	}
-
-	function incrementLocal() {
-		if (localInspirationsCount < 3) localInspirationsCount++;
-	}
-
-	function unlockAll() {
-		localInspirationsCount = 3;
-	}
-
-	function restart() {
-		localInspirationsCount = 0;
-	}
-
 	onMount(() => {
 		eventBus.on('updateStats', handleUpdateStats);
 		console.log($query.data);
@@ -77,157 +59,102 @@
 	</div>
 
 	<!-- Stats Display -->
-	<div class="flex absolute top-4 right-4 gap-6 items-start text-white">
+	<div class="flex absolute top-4 right-4 gap-6">
 		<div class="text-center">
-			<div class="text-3xl font-medium text-white">{$query.data.visionRank}</div>
-			<div class="text-xs text-tertiary-300">vision rank</div>
+			<div class="text-2xl font-medium text-white md:text-4xl">1</div>
+			<div class="text-sm text-tertiary-300 md:text-base">vision rank</div>
 		</div>
 		<div class="text-center">
-			<div class="text-3xl font-medium text-white">{$query.data.inspirations}</div>
-			<div class="text-xs text-tertiary-300">inspirations</div>
-		</div>
-		<div class="text-center">
-			<div class="text-3xl font-medium text-white">${$query.data.inspirations * 5}/m</div>
-			<div class="text-xs text-tertiary-300">stream potential</div>
-		</div>
-		<div class="text-center">
-			<div class="text-3xl font-medium text-white">{temperature}°</div>
-			<div class="text-xs text-tertiary-300">temp</div>
+			<div class="text-2xl font-medium text-white md:text-4xl">{$query.data.inspirations}</div>
+			<div class="text-sm text-tertiary-300 md:text-base">inspirations</div>
 		</div>
 	</div>
 
 	<!-- Main Content -->
-	<div class="flex relative z-10 flex-col h-full text-white">
-		<div class="flex flex-col flex-1 justify-center">
-			<!-- Time -->
-			<div class="text-center">
-				<h1 class="mb-4 text-9xl font-black tracking-wider h1">
-					{new Date().toLocaleTimeString('en-US', {
-						hour: '2-digit',
-						minute: '2-digit',
-						hour12: false
-					})}
-				</h1>
+	<div class="flex relative flex-col justify-center items-center p-4 w-full min-h-screen">
+		<!-- Time -->
+		<div class="mb-8 text-center">
+			<h1 class="text-6xl font-bold text-white md:text-8xl">
+				{new Date().toLocaleTimeString('en-US', {
+					hour: '2-digit',
+					minute: '2-digit',
+					hour12: false
+				})}
+			</h1>
+			<h2 class="mt-4 text-xl text-tertiary-300 md:text-4xl">
+				Be a magnet for joy, love and abundance.
+			</h2>
+		</div>
 
-				<!-- Welcome Message -->
-				<h2 class="mb-10 text-4xl text-tertiary-300">Be a magnet for joy, love and abundance.</h2>
-
-				<!-- Early Access Section -->
-				<div class="px-8 py-8 mx-auto max-w-4xl text-center rounded-xl bg-surface-900/50">
+		<!-- Early Access Section -->
+		<div class="mx-auto w-full max-w-sm md:max-w-2xl">
+			<div class="p-6 rounded-xl bg-surface-900/50 md:p-10">
+				{#if $showQRCode}
+					<div class="flex justify-center mb-6">
+						<QRCode
+							data={invitationLink}
+							backgroundColor="#141a4d"
+							color="#f0ede5"
+							shape="circle"
+							haveBackgroundRoundedEdges
+							logoPath="/logo.png"
+							logoSize="25"
+							logoPadding="4"
+						/>
+					</div>
+				{:else}
 					{#if inspirations < 3}
-						<p class="mb-8 text-2xl text-tertiary-300">
-							To unlock early access to Hominio, inspire 3 new Visioncreators.
+						<p class="mb-6 text-base text-center text-tertiary-300 md:text-xl">
+							To unlock early access to Hominio,<br />
+							inspire {3 - inspirations} more Visioncreator{3 - inspirations === 1 ? '' : 's'} to join
+							the waitlist.
 						</p>
 					{:else if inspirations === 3}
-						<p class="mb-8 text-2xl text-tertiary-300">
-							<span class="font-bold uppercase">Almost there! </span><br /> In January you receive your
-							early BETA invite.
+						<p class="mb-6 text-base text-center text-tertiary-300 md:text-xl">
+							<span class="font-bold uppercase">Almost there!</span><br />
+							In January you receive your early BETA invite.
 						</p>
-						<div class="flex gap-12 justify-center items-center mb-6">
-							{#each Array(3).fill(false) as _, i}
-								<div
-									class="p-6 rounded-xl {i < inspirations
-										? 'bg-secondary-500/40'
-										: 'bg-surface-900/60'}"
-								>
-									<Icon
-										icon={i < inspirations
-											? 'solar:user-bold-duotone'
-											: 'solar:lock-password-bold-duotone'}
-										class={i < inspirations ? 'text-secondary-300' : 'text-tertiary-300'}
-										width={48}
-									/>
-								</div>
-							{/each}
-						</div>
 					{:else}
-						<p class="mb-8 text-2xl text-tertiary-300">
+						<p class="mb-6 text-base text-center text-tertiary-300 md:text-xl">
 							Welcome to a new way of living and working<br />
 							enjoy playing with Hominio
 						</p>
-						<div class="absolute bottom-20 left-1/2 justify-center -translate-x-1/2">
-							<p
-								class="px-6 py-4 text-lg font-light whitespace-pre-line rounded-xl opacity-90 text-tertiary-400 bg-surface-900/60"
+					{/if}
+
+					<div class="flex gap-4 justify-center items-center mb-6 md:gap-10">
+						{#each Array(3).fill(false) as _, i}
+							<div
+								class="p-3 rounded-xl md:p-12 {i < inspirations
+									? 'bg-secondary-500/40'
+									: 'bg-surface-900/60'}"
 							>
-								To start talking to Hominio <br />long press the logo button.
-							</p>
-						</div>
-						<button
-							on:click={restart}
-							class="px-4 py-2 text-lg rounded-full bg-surface-900/60 text-tertiary-300 hover:bg-surface-900/80"
-						>
-							Restart
-						</button>
-					{/if}
-					{#if $showQRCode}
-						<div class="flex justify-center mb-6">
-							<QRCode
-								data={invitationLink}
-								backgroundColor="#141a4d"
-								color="#f0ede5"
-								shape="circle"
-								haveBackgroundRoundedEdges
-								logoPath="/logo.png"
-								logoSize="25"
-								logoPadding="4"
-							/>
-						</div>
-					{:else if inspirations < 3}
-						<div class="flex gap-12 justify-center items-center mb-6">
-							{#each Array(3).fill(false) as _, i}
-								<div
-									class="p-6 rounded-xl {i < inspirations
-										? 'bg-secondary-500/40'
-										: 'bg-surface-900/60'}"
-								>
-									<Icon
-										icon={i < inspirations
-											? 'solar:user-bold-duotone'
-											: 'solar:lock-password-bold-duotone'}
-										class={i < inspirations ? 'text-secondary-300' : 'text-tertiary-300'}
-										width={48}
-									/>
-								</div>
-							{/each}
-						</div>
-					{/if}
-					<div class="flex gap-6 justify-center items-center">
-						<button
-							on:click={decrementLocal}
-							class="px-4 py-2 text-lg rounded-full bg-surface-900/60 text-tertiary-300 hover:bg-surface-900/80"
-							disabled={inspirations === 0}
-						>
-							-1
-						</button>
-						<button
-							on:click={incrementLocal}
-							class="px-4 py-2 text-lg rounded-full bg-surface-900/60 text-tertiary-300 hover:bg-surface-900/80"
-							disabled={inspirations === 3}
-						>
-							+1
-						</button>
-						<button
-							on:click={() => {
-								localInspirationsCount = 4; // Switch to the last welcome view
-							}}
-							class="px-4 py-2 text-lg rounded-full bg-surface-900/60 text-tertiary-300 hover:bg-surface-900/80"
-						>
-							Unlock All
-						</button>
-						<button
-							on:click={copyInvitationLink}
-							class="px-4 py-2 text-lg rounded-full bg-surface-900/60 text-tertiary-300 hover:bg-surface-900/80"
-							disabled={$linkCopied}
-						>
-							{$linkCopied ? 'Copied!' : 'Copy Inspire Link'}
-						</button>
-						<button
-							on:click={toggleQRCode}
-							class="px-4 py-2 text-lg rounded-full bg-surface-900/60 text-tertiary-300 hover:bg-surface-900/80"
-						>
-							{$showQRCode ? 'Hide QR Code' : 'Show QR Code'}
-						</button>
+								<Icon
+									icon={i < inspirations
+										? 'solar:user-bold-duotone'
+										: 'solar:lock-password-bold-duotone'}
+									class={`w-12 h-12 md:w-16 md:h-16 ${
+										i < inspirations ? 'text-secondary-300' : 'text-surface-300'
+									}`}
+								/>
+							</div>
+						{/each}
 					</div>
+				{/if}
+
+				<div class="flex gap-3 justify-center items-center md:gap-6">
+					<button
+						on:click={copyInvitationLink}
+						class="px-4 py-2 text-sm rounded-full md:px-6 md:py-3 md:text-xl bg-surface-900/60 text-tertiary-300 hover:bg-surface-900/80"
+						disabled={$linkCopied}
+					>
+						{$linkCopied ? 'Copied!' : 'Copy Invite Link'}
+					</button>
+					<button
+						on:click={toggleQRCode}
+						class="px-4 py-2 text-sm rounded-full md:px-6 md:py-3 md:text-xl bg-secondary-500/40 text-secondary-300 hover:bg-secondary-500/60"
+					>
+						{$showQRCode ? 'Hide QR Code' : 'Show Invite Code'}
+					</button>
 				</div>
 			</div>
 		</div>
